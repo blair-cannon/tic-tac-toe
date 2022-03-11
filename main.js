8c1aef16027fd02ba43b5eba8fe55aca1bad42cd
Blair Preston <blair.preston@student.awesomeinc.org>
2022-03-11 12:26:29 +0000

    update reset button

--- main.js
+++ main.js
@@ -91,7 +91,6 @@ class View {
 
     resetDOM() {
         app.removeChild(this.tbl);
-        this.createBoard();
     }
     
 }
@@ -123,9 +122,6 @@ class Controller {
     }
 
     handlePlay = (e) => {      // arrow function inherits this from controller
-        // console.log(e.target);
-        // console.log(this);
-        // console.log(this.m.player);
         e.target.innerHTML = `${this.m.letter}`;  
         this.m.setBoard(e.target.id);   // update model to be used in checkForWin
         this.checkForWin();             // check for win before switching player to know who wins
@@ -143,45 +139,44 @@ class Controller {
     resetGame() {
         this.m.clearBoard();
         this.v.resetDOM();
-        console.log(this.m.board);
-        // init function to redo player names 
+        new Controller;
     }
     
 }
 new Controller;
 
     
-const init = function() { 
-    // create player 1 label and input box
-    var enterNames = document.createElement('p');
-    enterNames.setAttribute("class", "enterNames");
-    var label1 = document.createElement('label');
-    label1.setAttribute("for", "player1");
-    label1.innerHTML = "Player 1:";
-    var player1 = document.createElement('input');
-    player1.setAttribute("id", "player1");
-    player1.setAttribute("name", "player1");
-    player1.setAttribute("type", "text");
+// const init = function() { 
+//     // create player 1 label and input box
+//     var enterNames = document.createElement('p');
+//     enterNames.setAttribute("class", "enterNames");
+//     var label1 = document.createElement('label');
+//     label1.setAttribute("for", "player1");
+//     label1.innerHTML = "Player 1:";
+//     var player1 = document.createElement('input');
+//     player1.setAttribute("id", "player1");
+//     player1.setAttribute("name", "player1");
+//     player1.setAttribute("type", "text");
 
-    // create player 2 label and input box
-    var label2 = document.createElement('label');
-    label2.setAttribute("for", "player2");
-    label2.innerHTML = "Player 2:";
-    var player2 = document.createElement('input');
-    player2.setAttribute("id", "player2");
-    player2.setAttribute("name", "player2");
-    player2.setAttribute("type", "text");
-    label1.appendChild(player1);
-    label2.appendChild(player2);
-    enterNames.appendChild(label1);
-    enterNames.appendChild(label2);
-    document.body.appendChild(enterNames);
+//     // create player 2 label and input box
+//     var label2 = document.createElement('label');
+//     label2.setAttribute("for", "player2");
+//     label2.innerHTML = "Player 2:";
+//     var player2 = document.createElement('input');
+//     player2.setAttribute("id", "player2");
+//     player2.setAttribute("name", "player2");
+//     player2.setAttribute("type", "text");
+//     label1.appendChild(player1);
+//     label2.appendChild(player2);
+//     enterNames.appendChild(label1);
+//     enterNames.appendChild(label2);
+//     document.body.appendChild(enterNames);
 
-    // create button
-    var goButton = document.createElement('button');
-    goButton.setAttribute("class", "btn btn-secondary");
-    goButton.innerHTML = "Ready to Play!";
-    enterNames.appendChild(goButton);
+//     // create button
+//     var goButton = document.createElement('button');
+//     goButton.setAttribute("class", "btn btn-secondary");
+//     goButton.innerHTML = "Ready to Play!";
+//     enterNames.appendChild(goButton);
     
-}
+// }
 
